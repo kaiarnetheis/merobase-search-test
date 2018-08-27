d6012179b28f016012242aa5de6a632e3f7df83a
Kai-Arne Theis <ktheis@mail.uni-mannheim.de>
2018-08-27 18:58:04 +0200

    Changed test

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 
 public class ExampleTest {
 
-	@Test
-	public void exampleMethodTest() {
-		Example ex = new Example();
+	//@Test
+	//public void exampleMethodTest() {
+		//Example ex = new Example();
 		
 		//assertFalse(!ex.exampleMethod());
 		//assertTrue(ex.exampleMethod());
-	}
+	//}
 
 }
