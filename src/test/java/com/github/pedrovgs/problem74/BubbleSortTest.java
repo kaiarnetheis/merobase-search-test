0418e5954ed5164b5105e45e7ed456c4ee93e0d1
Kai-Arne Theis <ktheis@mail.uni-mannheim.de>
2018-08-30 17:17:16 +0200

    Added BubbleSortTest

---
+++ src/test/java/com/github/pedrovgs/problem74/BubbleSortTest.java
@@ -0,0 +1,14 @@
+package com.github.pedrovgs.problem74;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BubbleSortTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
